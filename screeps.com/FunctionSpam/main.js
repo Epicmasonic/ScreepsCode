dfa4fcda40e3ea0d0fcdb72b868b56969b4f8ddb
Epicmasonic <90917323+Epicmasonic@users.noreply.github.com>
2025-09-09 23:39:24 -0400

    Update main.js

--- screeps.com/FunctionSpam/main.js
+++ screeps.com/FunctionSpam/main.js
@@ -2,6 +2,19 @@ const harvester = require('(Creep role) Harvester');
 const summon = require('(Building actions) Spawn');
 
 module.exports.loop = function () {
+    new RoomVisual('E43S57').text('Pixel Progress: ' + Game.cpu.bucket / 100 + '%', -0.25, 0.25, {align: 'left'}); 
+    
+    if (Game.cpu.bucket >= 10000) {
+        Game.cpu.generatePixel();
+    }
+    
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if (creep.memory.role == 'Harvest') {
